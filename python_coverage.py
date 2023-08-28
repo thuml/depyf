aa5ddb57c7b8bd12852df738dabc063b1a9399c3
youkaichao <youkaichao@126.com>
2023-08-28 16:52:35 +0800

    fix

--- python_coverage.py
+++ python_coverage.py
@@ -17,8 +17,9 @@ for k in dis.opmap:
 
 print("Python version:", sys.version)
 if len(unsupported_opnames) == 0:
-    print("All opnames are supported!")
+    print("All opnames are considered!")
 else:
-    print(f"Total {len(unsupported_opnames)} unsupported opnames:")
+    print(f"Total {len(unsupported_opnames)} unconsidered opnames:")
     for k in unsupported_opnames:
         print(k)
+print('Note: some opnames are not supported, such as async, await, etc.')
