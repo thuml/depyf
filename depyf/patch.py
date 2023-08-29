ae6baee0430c6671d69a5fbab0a29e147b2b3248
youkaichao <youkaichao@126.com>
2023-08-29 17:37:01 +0800

    fix if

--- depyf/patch.py
+++ depyf/patch.py
@@ -1,12 +1,15 @@
 """Add a method to `dis.Instruction` to get jump target."""
 
 import dis
+import sys
+
+py311 = sys.version_info >= (3, 11)
 
 def get_jump_target(self: dis.Instruction):
     if self.opcode in dis.hasjabs:
         return self.argval
     elif self.opcode in dis.hasjrel:
-        return self.offset + self.argval
+        return self.offset + self.argval if not py311 else self.argval
     else:
         raise ValueError(f"Instruction {self.opname} does not have jump target")
 
