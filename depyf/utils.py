bd528a70a914fe55d5a42751517065bd58d6d3d2
youkaichao <youkaichao@126.com>
2023-09-04 12:06:29 +0800

    allow custom function name

--- depyf/utils.py
+++ depyf/utils.py
@@ -41,9 +41,11 @@ def generate_dot_table(header: str, rows: List[List[str]]) -> str:
     
     return "<\n" + html_str + "\n>"
 
-def get_function_signature(code_obj: CodeType) -> str:
+def get_function_signature(code_obj: CodeType, overwite_fn_name: Optional[str]=None) -> str:
     # Extract all required details from the code object
+    # Sometimes the code object does not have a name, e.g. when it is a lambda function, so we can overwrite it to be a valid name
     arg_names = code_obj.co_varnames[:code_obj.co_argcount]
     args_str = ', '.join(arg_names)
-    header = f"def {code_obj.co_name}({args_str}):\n"
+    fn_name = overwite_fn_name if overwite_fn_name is not None else code_obj.co_name
+    header = f"def {fn_name}({args_str}):\n"
     return header
