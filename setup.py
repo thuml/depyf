d1240ca922a86dcd262f2aaae98384ce8899ae9d
youkaichao <youkaichao@126.com>
2023-10-08 09:13:23 +0800

    setup install for pytorch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='depyf',
-    version='0.3.0',
+    version=open("VERSION.txt").read().strip(),
     description='Decompile python functions, from bytecode to source code!',
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
