5a636ecbf651e99b038612c82ff8e82570dc1c78
youkaichao <youkaichao@126.com>
2023-10-31 20:44:24 +0800

    add version check for pytorch

--- depyf/__init__.py
+++ depyf/__init__.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple, Dict, Union, Callable, Optional, Any
 from types import CodeType
+import warnings
 
 from .decompiler import Decompiler
 
@@ -11,6 +12,18 @@ def decompile(code: Union[CodeType, Callable]):
 
 
 import types
+
+try:
+    import torch
+    torch_version = torch.__version__
+    valid = ("dev" not in torch_version and torch_version >= "2.2") or ("dev" in torch_version and torch_version.split("dev")[-1] >= "20231020")
+    if not valid:
+        warnings.warn((
+            "Please use the nightly version of PyTorch to enable bytecode hooks.\n"
+            "PyTorch nightly can be installed by: `conda install pytorch-nightly::pytorch torchvision torchaudio -c pytorch-nightly`"
+        ))
+except ImportError:
+    pass
 
 
 def pytorch_bytecode_src_hook(code: types.CodeType, new_code: types.CodeType):
