385de470235064c7d4ddd86e7bd04a5d0466c6f4
youkaichao <youkaichao@126.com>
2023-10-31 20:46:58 +0800

    add test for aot eager

---
+++ tests/test_pytorch/test_debug_function_aot.py
@@ -0,0 +1,19 @@
+import torch
+from depyf.explain.backend import eager, aot_eager
+
+@torch.compile(backend=aot_eager)
+def toy_example(a, b):
+    x = a / (torch.abs(a) + 1)
+    if b.sum() < 0:
+        b = b * -1
+    return x * b
+
+import depyf
+# uncomment the following line to allow you to set breakpoints in the dumped source code
+# with depyf.prepare_debug(toy_example, "./dump_src_debug_function"):
+with depyf.prepare_debug(toy_example, "./dump_src_debug_function_aot", pause=False):
+    for _ in range(100):
+        toy_example(torch.randn(10, requires_grad=True), torch.randn(10, requires_grad=True))
+
+with depyf.debug():
+    toy_example(torch.randn(10, requires_grad=True), torch.randn(10, requires_grad=True))
