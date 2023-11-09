d1a57a75f30163077a84782c616ae5a3176ae278
youkaichao <youkaichao@126.com>
2023-11-09 20:26:21 +0800

    refactor code

---
+++ depyf/explain/enhance_logging.py
@@ -0,0 +1,40 @@
+import types
+from depyf.decompiler import Decompiler
+
+def pytorch_bytecode_src_hook(code: types.CodeType, new_code: types.CodeType):
+    import torch
+    bytecode_log = torch._logging.getArtifactLogger(
+        "torch._dynamo.convert_frame", "bytecode"
+    )
+    import logging
+
+    if bytecode_log.isEnabledFor(logging.DEBUG):
+        try:
+            decompiled_src = Decompiler(new_code).decompile()
+            bytecode_log.debug("possible source code:")
+            bytecode_log.debug(decompiled_src)
+        except Exception as e:
+            bytecode_log.debug("Decompilation fails due to: %s", str(e))
+        finally:
+            bytecode_log.debug(
+                "If you find the decompiled code is wrong,"
+                "please submit an issue at "
+                "https://github.com/thuml/depyf/issues."
+            )
+
+_handle = None
+
+def install():
+    import torch
+    global _handle
+    if _handle is not None:
+        return
+    _handle = torch._dynamo.convert_frame.register_bytecode_hook(pytorch_bytecode_src_hook)
+
+
+def uninstall():
+    global _handle
+    if _handle is None:
+        return
+    _handle.remove()
+    _handle = None
