254f0977fba3eb856726d925e54a6c128e10656a
youkaichao <youkaichao@126.com>
2023-11-10 19:01:52 +0800

    enable inductor debug

--- depyf/explain/enable_debugging.py
+++ depyf/explain/enable_debugging.py
@@ -81,6 +81,41 @@ def prepare_debug(func, dump_src_dir, pause=True):
     old_code = torch._dynamo.utils.lazy_format_graph_code.__code__
     torch._dynamo.utils.lazy_format_graph_code.__code__ = lazy_format_graph_code.__code__
 
+    from torch._inductor.codecache import PyCodeCache
+    old_inductor_fn = PyCodeCache.load_by_key_path
+
+    def new_inductor_fn(
+            key: str,
+            path: str,
+            linemap,
+            attrs,
+        ):
+        # hack the path to our dump_src_dir
+        src = open(path).read()
+        os.remove(path)
+
+        from torch._dynamo.bytecode_transformation import _unique_id_counter
+        from copy import copy
+        # torch.compile already called the next, we should add minus 1 to get the correct name
+        current_count = next(copy(_unique_id_counter)) - 1
+        func_name = "__compiled_fn_" + str(current_count)
+        count = 0
+        while True:
+            new_filepath = os.path.join(dump_src_dir, func_name + " kernel " + str(count) + ".py")
+            if not os.path.exists(new_filepath):
+                with open(new_filepath, "w") as f:
+                    f.write(src)
+                break
+            # might be a hash collision
+            existing_code = open(new_filepath).read()
+            if existing_code == src:
+                break
+            count += 1
+
+        path = new_filepath
+        return old_inductor_fn(key, path, linemap, attrs)
+
+    PyCodeCache.load_by_key_path = new_inductor_fn
     try:
         yield
     finally:
@@ -92,6 +127,7 @@ def prepare_debug(func, dump_src_dir, pause=True):
             f.write(full_src)
         torch.fx.graph_module._exec_with_source = old_func
         torch._dynamo.utils.lazy_format_graph_code.__code__ = old_code
+        PyCodeCache.load_by_key_path = old_inductor_fn
         if pause:
             input(f"Please check the full source code in {filename}, and set breakpoints for functions in {dump_src_dir} according to the hash value. Then press enter to continue.")
 
