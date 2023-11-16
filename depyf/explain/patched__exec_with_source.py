ba612c875f06871121a36f94febaaafee230880e
youkaichao <youkaichao@126.com>
2023-11-16 23:02:50 +0800

    move while to utils

--- depyf/explain/patched__exec_with_source.py
+++ depyf/explain/patched__exec_with_source.py
@@ -1,5 +1,6 @@
 def patched__exec_with_source(src: str, globals, co_fields=None):
     from depyf.explain.global_variables import data
+    from depyf.explain.utils import write_code_to_file_template
     dump_src_dir = data["dump_src_dir"]
     unpatched__exec_with_source = data["unpatched__exec_with_source"]
     unpatched__exec_with_source(src, globals, co_fields)
@@ -9,16 +10,5 @@ def patched__exec_with_source(src: str, globals, co_fields=None):
     import os
     hash_value = hashlib.md5(src.encode()).hexdigest()
     src = "# " + key + src
-    count = 0
-    while True:
-        filename = f"{dump_src_dir}/fx_graph_code_" + hash_value + "_" + str(count) + ".py"
-        if not os.path.exists(filename):
-            with open(filename, "w") as f:
-                f.write(src)
-            break
-        # might be a hash collision
-        existing_code = open(filename).read()
-        if existing_code == src:
-            break
-        count += 1
+    filename = write_code_to_file_template(src, f"{dump_src_dir}/fx_graph_code_" + hash_value + "_" + "%s" + ".py")
     exec(compile(src, filename, "exec"), globals)
--- depyf/explain/patched_load_by_key_path.py
+++ depyf/explain/patched_load_by_key_path.py
@@ -5,6 +5,7 @@ def patched_load_by_key_path(
         attrs,
     ):
     from depyf.explain.global_variables import data
+    from depyf.explain.utils import write_code_to_file_template
     dump_src_dir = data["dump_src_dir"]
     unpatched_load_by_key_path = data["unpatched_load_by_key_path"]
     import os
@@ -17,18 +18,6 @@ def patched_load_by_key_path(
     # torch.compile already called the next, we should add minus 1 to get the correct name
     current_count = next(copy(_unique_id_counter)) - 1
     func_name = "__compiled_fn_" + str(current_count)
-    count = 0
-    while True:
-        new_filepath = os.path.join(dump_src_dir, func_name + " kernel " + str(count) + ".py")
-        if not os.path.exists(new_filepath):
-            with open(new_filepath, "w") as f:
-                f.write(src)
-            break
-        # might be a hash collision
-        existing_code = open(new_filepath).read()
-        if existing_code == src:
-            break
-        count += 1
-
+    new_filepath = write_code_to_file_template(src, os.path.join(dump_src_dir, func_name + " kernel " + "%s" + ".py"))
     path = new_filepath
     return unpatched_load_by_key_path(key, path, linemap, attrs)
