4d4c685b618d47d5a5925797fe71c9bad54f94e5
youkaichao <youkaichao@126.com>
2023-11-17 21:08:55 +0800

    update pytorch test

---
+++ tests/test_pytorch/test_pytorch.py
@@ -0,0 +1,64 @@
+import torch
+
+def toy_function(a, b):
+    x = a / (torch.abs(a) + 1)
+    if b.sum() < 0:
+        b = b * -1
+    return x * b
+
+class ToyModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, a, b):
+        x = a / (torch.abs(a) + 1)
+        if b.sum() < 0:
+            b = b * -1
+        return x * b
+
+
+import os
+backend = os.environ.get("TORCH_COMPILE_BACKEND", "eager")
+requires_grad = os.environ.get("REQUIRES_GRAD", "0") != "0"
+dynamic_shape = os.environ.get("DYNAMIC_SHAPE", "0") != "0"
+compile_type = os.environ.get("COMPILE_TYPE", "function")
+usage_type = os.environ.get("USAGE_TYPE", "debug")
+
+assert backend in ["eager", "aot_eager", "inductor"]
+
+input1 = torch.randn(10, requires_grad=requires_grad), torch.randn(10, requires_grad=requires_grad)
+
+input2 = torch.randn(8, requires_grad=requires_grad), torch.randn(8, requires_grad=requires_grad)
+
+if compile_type == "function":
+    target = toy_function
+elif compile_type == "module":
+    target = ToyModule()
+else:
+    raise ValueError("Unknown compile_type: {}".format(compile_type))
+
+target = torch.compile(target, backend=backend)
+
+def warmup():
+    for _ in range(100):
+        target(*input1)
+        if dynamic_shape:
+            target(*input2)
+
+def call():
+    target(*input1)
+
+
+if usage_type == "dump":
+    from depyf.explain import dump_src
+    warmup()
+    src = dump_src(target)
+    with open(f"./depyf_output/{usage_type}_{compile_type}_{backend}.py", "w") as f:
+        f.write(src)
+elif usage_type == "debug":
+    import depyf
+    with depyf.prepare_debug(target, f"./depyf_output/{usage_type}_{compile_type}_{backend}"):
+        warmup()
+
+    with depyf.debug():
+        call()
