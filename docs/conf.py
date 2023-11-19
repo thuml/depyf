6a15d903aa6b31a083e1ef1257afd613517776e0
youkaichao <youkaichao@126.com>
2023-11-19 11:35:09 +0800

    add doc

--- docs/conf.py
+++ docs/conf.py
@@ -6,14 +6,26 @@ project = 'depyf'
 author = 'Kaichao You et al.'
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
     'sphinx.ext.todo',
 ]
+
 
 source_encoding = 'utf-8'
 
 master_doc = 'index'
+html_theme = "sphinx_rtd_theme"
 language = "en-US"
 
 todo_include_todos = True
+
+html_static_path = ["_static"]
+
+html_logo = "_static/images/depyf-logo.png"
