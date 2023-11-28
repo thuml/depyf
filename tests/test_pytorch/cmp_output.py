70ac60dd2b53065dfef0cc87167dfa9f09440c3e
youkaichao <youkaichao@126.com>
2023-11-28 17:26:49 +0800

    add more assert message

--- tests/test_pytorch/cmp_output.py
+++ tests/test_pytorch/cmp_output.py
@@ -6,7 +6,7 @@ output_files.sort()
 expected_files = glob.glob("tests/depyf_output/*/*.py")
 expected_files.sort()
 
-assert len(output_files) == len(expected_files)
+assert len(output_files) == len(expected_files), f"len(output_files)={len(output_files)}, len(expected_files)={len(expected_files)}"
 
 for output_file, expected_file in zip(output_files, expected_files):
     with open(output_file, "r") as f:
@@ -19,4 +19,6 @@ for output_file, expected_file in zip(output_files, expected_files):
         for line in f:
             if line.strip() and not line.strip().startswith("#"):
                 expected_lines.append(line.strip())
-    assert output_lines == expected_lines
+    assert len(output_lines) == len(expected_lines), f"len(output_lines)={len(output_lines)}, len(expected_lines)={len(expected_lines)}"
+    for output_line, expected_line in zip(output_lines, expected_lines):
+        assert output_line == expected_line, f"output_line={output_line}, expected_line={expected_line}"
