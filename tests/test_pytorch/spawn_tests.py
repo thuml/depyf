114d4cd8cfeea31526e77ff8a2a2ec6d0ae7cdcd
youkaichao <youkaichao@126.com>
2023-11-29 19:51:44 +0800

    use python for tests

---
+++ tests/test_pytorch/spawn_tests.py
@@ -0,0 +1,25 @@
+import multiprocessing
+import subprocess
+import os
+
+def task():
+    subprocess.run("python tests/test_pytorch/test_pytorch.py", shell=True)
+
+if __name__ == '__main__':
+
+    for TORCH_COMPILE_BACKEND in ["eager", "aot_eager", "inductor"]:
+        for REQUIRES_GRAD in ["0", "1"]:
+            for DYNAMIC_SHAPE in ["0", "1"]:
+                for COMPILE_TYPE in ["function", "module"]:
+                    for USAGE_TYPE in ["debug", "dump"]:
+                        os.environ["TORCH_COMPILE_BACKEND"] = TORCH_COMPILE_BACKEND
+                        os.environ["REQUIRES_GRAD"] = REQUIRES_GRAD
+                        os.environ["DYNAMIC_SHAPE"] = DYNAMIC_SHAPE
+                        os.environ["COMPILE_TYPE"] = COMPILE_TYPE
+                        os.environ["USAGE_TYPE"] = USAGE_TYPE
+                        ps = [multiprocessing.Process(target=task) for i in range(3)]
+                        for p in ps:
+                            p.start()
+                        for p in ps:
+                            p.join()
+                        
