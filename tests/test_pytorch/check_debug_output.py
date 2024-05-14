2754176dbe12176e4da7aaf0af21f11c8d3f3a33
youkaichao <youkaichao@126.com>
2024-05-14 16:19:39 -0700

    update tests

--- tests/test_pytorch/check_debug_output.py
+++ tests/test_pytorch/check_debug_output.py
@@ -3,7 +3,7 @@ count = 0
 for line in open('tests/test_pytorch/debug_output.txt'):
     # when we use `pdb` and hit breakpoint, we will see `->` in the output
     # the first `->` is from `depyf.debug` and the second `->` is from user input
-    if line.startswith('->'):
+    if line.startswith('->') or line.startswith('>'):
         count += 1
 
 assert count == 2
