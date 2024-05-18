41ba5331ec100e4257360a4b1b97703f760fd77c
youkaichao <youkaichao@126.com>
2024-05-17 22:40:46 -0700

    add simple graph test

---
+++ tests/test_pytorch/test_simple_graph.py
@@ -0,0 +1,13 @@
+import torch
+
+@torch.compile
+def fn():
+    x = torch.randn(1, 10, requires_grad=True)
+    y = torch.randn(10, 1)
+    loss = torch.mm(x, y).sum()
+    loss.backward()
+    return x.grad
+
+import depyf
+with depyf.prepare_debug("./simple_output"):
+    fn()
