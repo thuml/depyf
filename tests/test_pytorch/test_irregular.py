3d2076e6a3dfffdb0bb63aacf3f31bfd417678b7
youkaichao <youkaichao@126.com>
2024-08-05 12:26:31 -0700

    add tests

---
+++ tests/test_pytorch/test_irregular.py
@@ -0,0 +1,37 @@
+def f():
+    print(len("Hello, world!"))
+import dis
+code = f.__code__
+
+from types import CodeType
+
+class Tmp:
+    def __init__(self):
+        self.called_count = 0
+    
+    def __len__(self):
+        self.called_count += 1
+        return 1
+
+tmp = Tmp()
+
+co_consts = code.co_consts
+# replace the const string with the globals() dict
+new_consts = tuple(x if x is None else tmp for x in co_consts)
+
+new_code = code.replace(co_consts=new_consts)
+
+import torch
+
+def convert_hook(old_code, compiled_code):
+    if old_code is code:
+        return new_code
+
+handle = torch._dynamo.convert_frame.register_bytecode_hook(convert_hook)
+f = torch.compile(f)
+
+import depyf
+with depyf.prepare_debug("test_irregular"):
+    f()
+    f()
+    assert tmp.called_count == 2
