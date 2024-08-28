a12f00fe8e684d8c1f53ab40d652ac93ffe01d72
youkaichao <youkaichao@126.com>
2024-08-27 17:25:44 -0700

    remove stale code

--- depyf/explain/__init__.py
+++ depyf/explain/__init__.py
@@ -10,16 +10,6 @@ def _extract_artifacts(original_code: CodeType, module):
     result = DynamoOptimizationResult(original_code, None, module)
     return result
 
-
-def _collect_compiled_subgraphs(result: DynamoOptimizationResult):
-    compiled_subgraphs = {
-        entry.compiled_subgraph_proxy.name: entry.compiled_subgraph for entry in result.compiled_code_entries}
-    for entry in result.compiled_code_entries:
-        for func in entry.referenced_global_functions.values():
-            ans = _collect_compiled_subgraphs(func)
-            compiled_subgraphs.update(ans)
-    return compiled_subgraphs
-
 def dump_src(original_code: CodeType, module):
     from depyf.explain.global_variables import data
     assert data["is_inside_prepare_debug"], "`dump_src` must be used inside `depyf.prepare_debug`."
