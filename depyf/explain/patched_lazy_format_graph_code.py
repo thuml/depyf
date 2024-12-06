810a8fb599471ac1a9c6ed2b5d4b558def3bd1ce
youkaichao <youkaichao@gmail.com>
2024-12-06 15:39:36 -0800

    add import torch
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- depyf/explain/patched_lazy_format_graph_code.py
+++ depyf/explain/patched_lazy_format_graph_code.py
@@ -27,7 +27,7 @@ def patched_lazy_format_graph_code(name, gm, maybe_id=None, **kwargs):
     use_gm = True
 
     # use `print_readable` because it can include submodules
-    src = "from __future__ import annotations\n" + \
+    src = "from __future__ import annotations\nimport torch\n" + \
         gm.print_readable(print_output=False)
     src = src.replace("<lambda>", "GraphModule")
     try:
