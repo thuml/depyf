d7d6bc53d42170f0d7871162dd8570fe42d1f2bc
youkaichao <youkaichao@gmail.com>
2025-10-08 01:27:01 +0800

    update tests
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- tests/test_pytorch/test_no_graph.py
+++ tests/test_pytorch/test_no_graph.py
@@ -2,16 +2,9 @@ import torch
 
 @torch.compile
 def f(x):
-    s = "Hello" + x
-    print(s)
-
-@torch.compile
-def g(a, b):
-    assert a
-    print(b)
-    [a for _ in [None]]
+    s = "hello " + x
+    return s
 
 import depyf
 with depyf.prepare_debug("./temp_output"):
     f(", world!")
-    g(1, 2)
